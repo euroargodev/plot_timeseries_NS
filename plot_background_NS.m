c7571f93ecdc070a464ac9b52d3a043d773c6c12
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2021-08-10 19:40:07 +0200

    Change way to plot the maps and profile selection to accomodate modes and flags

--- plot_background_NS.m
+++ plot_background_NS.m
@@ -1,15 +1,17 @@
-function plot_background_NS
+function plot_background_NS(lonlims,latlims)
 load interp_fh_NS.mat basins data
-
+warning('off','MATLAB:handle_graphics:Patch:NumColorsNotEqualNumVertsOrNumFacesException')
+%% colormap (from colorbrewer)
+cm=[250,250,250;141,211,199;255,255,179;190,186,218;251,128,114];
+cm=cm./255;
 %% plot
-latlims=[63 79];
-lonlims=[-20 10];
 m_proj('lambert','lat',latlims,'long',lonlims,'rect','on');
-figure('color','w')
-m_gshhs_i('patch',[.7 .7 .7],'edgecolor','k');
 hold on
-m_contourf(convertlon(data.long,180),data.lat,basins)
+m_contourf(convertlon(data.long,180),data.lat,basins);shading flat
 m_etopo2('contour',[-6000:500:-500],'linecolor',5*[0.1 0.1 0.1]);
-m_grid('xtick',[-10:10:20],'ytick',[60:5:80])
-caxis([0 4]);colormap(jet(5))
+xt=floor(lonlims(1)/10)*10:5:ceil(lonlims(2)/10)*10;
+yt=floor(latlims(1)/10)*10:5:ceil(latlims(2)/10)*10;
+m_grid('xtick',xt,'ytick',yt)
+caxis([0 4]);colormap(cm);
+m_gshhs_i('patch',[.7 .7 .7],'edgecolor','k');
 %savefig background_basins.fig
--- plot_classfloatprof.m
+++ plot_classfloatprof.m
@@ -1,22 +1,44 @@
 function plot_classfloatprof(data,gs,ip,lb,nb)
-% plot 
-plot_background_NS
+% plot all NS
+subplot(1,2,1)
+latlims=[63 79];
+lonlims=[-20 10];
+plot_background_NS(lonlims,latlims)
 hold on
-if isfield(data,'Flat')
-   m_plot(data.Flong,data.Flat,'ob','MarkerFaceColor','b','MarkerSize',2)
-   m_plot(data.long,data.lat,'ok','MarkerFaceColor','k','MarkerSize',2)
-   title('In blue deleted cycles')
-   plot_background_NS
-   m_plot(data.Flong,data.Flat,'ob','MarkerFaceColor','b','MarkerSize',2)
-   m_plot(data.long,data.lat,'ok','MarkerFaceColor','k','MarkerSize',2)
-   hold on
-end
-m_plot(data.long,data.lat,'.k')
+% plot all
+m_plot(data.long,data.lat,'.-k')
+% plot mode
+m_plot(data.long(data.modenum==2),data.lat(data.modenum==2),'o',...
+    'MarkerFaceColor',[0 0.45 0.74],'MarkerEdgeColor',[0 0.45 0.74],'MarkerSize',2)% dmode blue
+m_plot(data.long(data.modenum<2),data.lat(data.modenum<2),'o',...
+    'MarkerFaceColor',[0.85 0.33 0.10],'MarkerEdgeColor',[0.85 0.33 0.10],'MarkerSize',2) % rmode red
+
+title({'Prof. modes: red - available only in R or A mode';...
+    'blue - available in D mode. Prof. in each basin: ';...
+['GS (green)-' num2str(numel(gs)) ' IS (yellow)-' num2str(numel(ip)) ...
+     ' LB (lillac)-' num2str(numel(lb)) ' NB (orange)-' num2str(numel(nb))]})
+
+ 
+subplot(1,2,2)
+lonlims=[min(data.long)-0.5 max(data.long)+0.5];
+latlims=[min(data.lat)-0.5 max(data.lat)+0.5];
+plot_background_NS(lonlims,latlims)
+% plot prof avaliability (flag)
+% plot rmode 
+m_plot(data.long(data.rind),data.lat(data.rind),'o',...
+    'MarkerFaceColor',[0.85 0.33 0.10],'MarkerEdgeColor','k','MarkerSize',4) % rmode red
+% plot dmode
+m_plot(data.long(data.dind),data.lat(data.dind),'o',...
+    'MarkerFaceColor',[0 0.45 0.74],'MarkerEdgeColor','k','MarkerSize',4)% dmode blue
 
 hold on
-if isempty(gs)==0;m_plot(data.long(gs),data.lat(gs),'ok');end
-if isempty(ip)==0;m_plot(data.long(ip),data.lat(ip),'ok');end
-if isempty(lb)==0;m_plot(data.long(lb),data.lat(lb),'ok');end
-if isempty(nb)==0;m_plot(data.long(nb),data.lat(nb),'ok');end
-title({'Classified profiles (encircled) - Deleted in blue ';[' GS (cyan): ' num2str(numel(gs)) ' IS (green): ' num2str(numel(ip)) ...
-    ' LB (yellow): ' num2str(numel(lb)) ' NB (orange): ' num2str(numel(nb))]})
+if isempty(gs)==0;m_plot(data.long(gs),data.lat(gs),'.k','MarkerSize',4);end
+if isempty(ip)==0;m_plot(data.long(ip),data.lat(ip),'.k','MarkerSize',4);end
+if isempty(lb)==0;m_plot(data.long(lb),data.lat(lb),'.k','MarkerSize',4);end
+if isempty(nb)==0;m_plot(data.long(nb),data.lat(nb),'.k','MarkerSize',4);end
+
+title({'Prof. availability with selected flags: red - only in R or A mode';...
+    'blue - in D mode. Prof. inside a basin: dot inside';})
+
+
+
