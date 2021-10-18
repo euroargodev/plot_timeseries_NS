aa3746969539e9ddc267d2bc7ffa931df89edc81
Ingrid M. Angel Benavides <ingrid.angel@bsh.de>
2021-10-18 13:36:47 +0200

    Add WMO number to plots' titles and fixed color consistency across plots

--- plot_eachbasinprof.m
+++ plot_eachbasinprof.m
@@ -8,7 +8,7 @@ else
         floatdata.ipres=ipres;
         load interp_ctdrdb_NS.mat gs_int
         nplot=plot_timeseries(preslevel,year,gs_int,floatdata,mode,bname);
-        title({[bname ' basin (' num2str(preslevel)  ' db) - Mode ' upper(mode) ' - N cycles = ' num2str(nplot(1))];...
+        title({['WMO ' num2str(data.wmo) ' - ' bname ' basin (' num2str(preslevel)  ' db) - Float in ' upper(mode) ' mode - N cycles = ' num2str(nplot(1))];...
             ['N salinity flag 2 (+) =  ' num2str(nplot(2)) ' - N salinity flag 3 (*) =  ' num2str(nplot(3))]})
     end
     
@@ -18,7 +18,7 @@ else
         floatdata.ipres=ipres;
         load interp_ctdrdb_NS.mat ip_int
         nplot=plot_timeseries(preslevel,year,ip_int,floatdata,mode,'IP');
-        title({[bname ' basin (' num2str(preslevel)  ' db) - Mode ' upper(mode) ' - N cycles = ' num2str(nplot(1))];...
+        title({['WMO ' num2str(data.wmo) ' - ' bname ' basin (' num2str(preslevel)  ' db) - Float in ' upper(mode) ' mode - N cycles = ' num2str(nplot(1))];...
             ['N salinity flag 2 (+) =  ' num2str(nplot(2)) ' - N salinity flag 3 (*) =  ' num2str(nplot(3))]})
     end
     if isempty(lb)==0
@@ -27,7 +27,7 @@ else
         floatdata.ipres=ipres;
         load interp_ctdrdb_NS.mat lb_int
         nplot=plot_timeseries(preslevel,year,lb_int,floatdata,mode,bname);
-        title({[bname ' basin (' num2str(preslevel)  ' db) - Mode ' upper(mode) ' - N cycles = ' num2str(nplot(1))];...
+        title({['WMO ' num2str(data.wmo) ' - ' bname ' basin (' num2str(preslevel)  ' db) - Float in ' upper(mode) ' mode - N cycles = ' num2str(nplot(1))];...
             ['N salinity flag 2 (+) =  ' num2str(nplot(2)) ' - N salinity flag 3 (*) =  ' num2str(nplot(3))]})
     end
     if isempty(nb)==0
@@ -36,7 +36,7 @@ else
         floatdata.ipres=ipres;
         load interp_ctdrdb_NS.mat nb_int
         nplot=plot_timeseries(preslevel,year,nb_int,floatdata,mode,bname);
-        title({[bname ' basin (' num2str(preslevel)  ' db) - Mode ' upper(mode) ' - N cycles = ' num2str(nplot(1))];...
+        title({['WMO ' num2str(data.wmo) ' - ' bname ' basin (' num2str(preslevel)  ' db) - Float in ' upper(mode) ' mode - N cycles = ' num2str(nplot(1))];...
             ['N salinity flag 2 (+) =  ' num2str(nplot(2)) ' - N salinity flag 3 (*) =  ' num2str(nplot(3))]})
     end
 end
--- plot_timeseries.m
+++ plot_timeseries.m
@@ -1,6 +1,8 @@
 function nplot=plot_timeseries(preslevel,year,basin,float,mode,basin_name)
 year=10000000000*year;
 
+cm=lines(2);
+
 basin.daten=dates2daten(basin.dates);
 pr=find(basin.ipres==preslevel);
 f=find(basin.dates>year);
@@ -25,14 +27,14 @@ pr=find(float.ipres==preslevel);
 f=find(float.dates>year);
 
 if strcmp(mode,'r')
-    plot(float.daten(f),float.isalr(pr,f),'ok','MarkerSize',6,'MarkerFaceColor','k')
+    plot(float.daten(f),float.isalr(pr,f),'ok','MarkerSize',6,'MarkerFaceColor',cm(2,:))
     f2=intersect(f,find(float.isalr_flag(pr,:)==2));% flag 2
     plot(float.daten(f2),float.isalr(pr,f2),'+w','MarkerSize',3)
     f3=intersect(f,find(float.isalr_flag(pr,:)==3));% flag 3
     plot(float.daten(f3),float.isalr(pr,f3),'*w','MarkerSize',3)
     nplot=[sum(isfinite(float.isalr(pr,f))) sum(isfinite(float.isalr(pr,f2))) sum(isfinite(float.isalr(pr,f3)))];
 elseif strcmp(mode,'d')
-    plot(float.daten(f),float.isald(pr,f),'ok','MarkerSize',6,'MarkerFaceColor','k')
+    plot(float.daten(f),float.isald(pr,f),'ok','MarkerSize',6,'MarkerFaceColor',cm(1,:))
     f2=intersect(f,find(float.isald_flag(pr,:)==2));% flag 2
     plot(float.daten(f2),float.isald(pr,f2),'+w','MarkerSize',3)
     f3=intersect(f,find(float.isald_flag(pr,:)==3));% flag 3
@@ -40,7 +42,7 @@ elseif strcmp(mode,'d')
     nplot=[sum(isfinite(float.isald(pr,f))) sum(isfinite(float.isald(pr,f2))) sum(isfinite(float.isald(pr,f3)))];
     
     if isempty(find(abs(float.isalr(pr,f)-float.isald(pr,f))>0, 1))==0
-        plot(float.daten(f),float.isalr(pr,f),'ok','MarkerSize',6,'MarkerFaceColor','r')
+        plot(float.daten(f),float.isalr(pr,f),'ok','MarkerSize',6,'MarkerFaceColor',cm(2,:))
         f2=intersect(f,find(float.isalr_flag(pr,:)==2));% flag 2
         plot(float.daten(f2),float.isalr(pr,f2),'+w','MarkerSize',3)
         f3=intersect(f,find(float.isalr_flag(pr,:)==3));% flag 3
@@ -51,9 +53,13 @@ end
 grid on
 datetick('x','yyyy','keeplimits')
 if isempty(find(abs(float.isalr(pr,f)-float.isald(pr,f))>0, 1))==0
-    legend('Basin','Float','Float in R mode','Location','northoutside','Orientation','horizontal')
+    legend('Basin','Float in D mode ','Float in R/A mode','Location','northoutside','Orientation','horizontal')
 else
-    legend('Basin','Float','Location','northoutside','Orientation','horizontal')
+    if strcmp(mode,'d')
+        legend('Basin','Float in D mode','Location','northoutside','Orientation','horizontal')
+    elseif strcmp(mode,'r')
+        legend('Basin','Float in R/A mode','Location','northoutside','Orientation','horizontal')
+    end        
 end
 xx=xlim;yy=ylim;
 set(findall(gcf,'-property','FontSize'),'FontSize',12)
